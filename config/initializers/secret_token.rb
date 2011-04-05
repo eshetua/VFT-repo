a5fe79156063e93ec6a87982e704d0f38b0165f9
eshetu@approbatics.com <Valued Customer@.(none)>
2011-04-05 10:36:11 +0300

    rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VFTRepo::Application.config.secret_token = 'f518c1bd2fc82eac208660860197fc183030fe840ff6ef83fed3262bfa9841e24b937c8c74369e3f5735eee360cf461bf4495073e6b50439ae210b0fc7dd851e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VFTRepo::Application.config.session_store :cookie_store, :key => '_VFT-repo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VFTRepo::Application.config.session_store :active_record_store
