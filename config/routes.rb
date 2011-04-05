779562135c61e4d3230e00cb94566e3f942ae4e6
eshetu@approbatics.com <Valued Customer@.(none)>
2011-04-05 16:43:08 +0300

    Added some ajaxy thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 VFTRepo::Application.routes.draw do
+  resources :subscribers, :only => [:create, :index]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ VFTRepo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "subscribers#index"
 
   # See how all your routes lay out with "rake routes"
 
